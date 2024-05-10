31a304b9e5ba0679ddc3c7ceda8442d8e504715f
csharp404 <yousefjobsdeve@gmail.com>
2024-05-10 20:26:31 +0300

    Add files via upload

---
+++ GuessTheRandomNumber.cs
@@ -0,0 +1,49 @@
+﻿// Yousef
+// [05/10/2024]
+//                  ___________
+//                 /           \
+//                 |  R.I.P.   |
+//                 |   _____   |
+//                 |  /     \  |
+//                 | | () () | |
+//                 |  \  ^  /  |
+//                 |   |||||   |
+//                 |   |||||   |
+//                 |   |||||   |
+//                 |   |||||   |
+//                 |   |||||   |
+//                 |___|||||___|
+//                 \___|||||___/
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Random random = new Random();
+        int ran = random.Next(1, 50);
+        int guess = -1;
+
+        do
+        {
+            Console.Write("The numbre you expect ? ");
+            if (int.TryParse(Console.ReadLine(), out int guessed))
+            {
+                guess = guessed;
+            }
+            if (guessed > ran)
+            {
+                Console.WriteLine("The number is less than what you expected.");
+            }
+            else if (guessed < ran)
+            {
+                Console.WriteLine("The number is more than what you expected.");
+            }
+            if (guessed == ran)
+            {
+                Console.WriteLine("Congratulations, you guessed the number right!");
+                break;
+            }
+        } while (guess != ran);
+
+    }
+}
